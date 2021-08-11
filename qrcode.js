0047afb1aec4670c486e293a4e1049c8a5c13695
Mikayla George <mikayla@hey.com>
2021-08-10 23:11:40 -0700

    add some js and html for random qr whatever

---
+++ qrcode.js
@@ -0,0 +1,55 @@
+const rand = (min = 0, max) => {
+    return Math.round(Math.random() * (max - min)) + min
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const canvasse = document.getElementById("qrcanv");
+    const contexte = canvasse.getContext("2d");
+    
+    contexte.fillStyle = 'rgb(200, 0, 0)';
+    contexte.fillRect(10, 10, 50, 50);
+    console.log("document loaded lol");
+
+    // a dummy code g enerator for the fun
+    const generateCode = (input) => {
+        const arraySize = rand(40,40);
+        console.log("arraySize", arraySize);
+        let ofArrays = [];
+        for(let i = 0; i < arraySize; i++) {
+            ofArrays[i] = [];
+            for(let j = 0; j < arraySize; j++) {
+                ofArrays[i][j] = rand(0,1);
+            }
+        }
+        return ofArrays;
+    }
+
+    const drawQRCode = (context, array) => {
+        array.forEach((row, rindex) => {
+            row.forEach((element, eindex) => {
+                if ( element == 1 ) {
+                    context.fillRect(
+                        rindex * 10,
+                        eindex * 10,
+                        10,
+                        10
+                    )
+                }
+            })
+        })
+    }
+
+    const redraw = () => {
+        console.log("redrawing");
+        const x = Math.floor(Math.random() * 400);
+        const y = Math.floor(Math.random() * 400);
+        contexte.fillRect(x, y, 20, 20)
+        // console.log(generateCode());
+        contexte.clearRect(0, 0, 400, 400)
+        drawQRCode(contexte, generateCode())
+        console.log(generateCode());
+    }
+
+    const inputElement = document.getElementById("textual");
+    inputElement.addEventListener('input', redraw);
+});
